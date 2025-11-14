2cccd2d3ab1f927552df629324c213ba4b286c7b
2006nicolas2023 <149892344+2006nicolas2023@users.noreply.github.com>
2025-11-14 13:07:45 -0500

    Add files via upload
    proyecto

---
+++ homesegurity/homesegurity/src/main/java/com/proyecto/homesegurity/HomesegurityApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.homesegurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesegurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesegurityApplication.class, args);
+	}
+
+}
---
+++ homesegurity/homesegurity/src/test/java/com/proyecto/homesegurity/HomesegurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.homesegurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesegurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
